821c7cce19aba34bc353cbef46e2eb652a609f13
MichaelFelixChan <krisnariyandi_2226240155@mhs.mdp.ac.id>
2025-04-23 18:37:49 +0700

    Update : tampilan posts dengan firestore

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,15 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+// untuk mengubah tanggal pada database dan menampilkan seperti timeStamp(5 menit yang lalu, 60 detik yang lalu)
+  String formatTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final diff = now.difference(dateTime);
+    if (diff.inSeconds < 60) {
+      return '${diff.inSeconds} secs ago';
+    } else if (diff.inMinutes < 60) {
+      return '${diff.inMinutes} mins ago';
+    } else if (diff.inHours < 24) {
+      return '${diff.inHours} hrs ago';
+    } else {
+      return DateFormat('dd/MM/yyyy').format(dateTime);
+    }
+  }
 
   Future<void> signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
 
     Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => SignInScreen()));
+        MaterialPageRoute(builder: (context) => SignInScreen()));
   }
 
   @override
@@ -20,14 +37,64 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         actions: [
           IconButton(
-            onPressed: (){
-              signOut(context);
-            }, 
-            icon: const Icon(Icons.logout)),
+              onPressed: () {
+                signOut(context);
+              },
+              icon: const Icon(Icons.logout)),
         ],
       ),
-      body: const Center(
-        child: Text("You have logged in"),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("posts")
+            // .orderBy('createdAt', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          // jika data tidak ada
+          if (!snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
+          // docs = bertipe list
+          final posts = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final data = posts[index].data();
+              final imageBase64 = data['image'];
+              final description = data['description'];
+              //final createdAtStr = data['createdAt'];
+              final fullName = data['fullName'] ?? 'Anonim';
+
+              //parse ke DateTime
+              //final createdAt = DateTime.parse(createdAtStr);
+              return Card(
+                margin: const EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      fullName,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      description ?? '',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
