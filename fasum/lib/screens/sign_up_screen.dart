abaff410f181f24eb5daa79d364995034e0cac3f
Pysager-0 <krisnariyandi_2226240155@mhs.mdp.ac.id>
2025-04-30 18:37:07 +0700

    update

--- fasum/lib/screens/sign_up_screen.dart
+++ fasum/lib/screens/sign_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -10,12 +11,14 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _fullNameController = TextEditingController();
+
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Registrasi Akun'),
       ),
@@ -23,6 +26,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            TextField(
+              controller: _fullNameController,
+              decoration: const InputDecoration(labelText: 'Full Name'),
+            ),
             TextField(
               controller: _emailController,
               decoration: const InputDecoration(labelText: 'Email'),
@@ -66,10 +73,19 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
     // Auntentikasi
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      final newUser =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(newUser.user!.uid)
+          .set({
+        'fullName': _fullNameController.text,
+        'email': _emailController.text,
+        'createdAt': Timestamp.now()
+      });
       if (mounted) {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
